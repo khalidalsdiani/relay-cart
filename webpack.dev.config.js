31e7955855f40770716785e50d30dd45d2596129
soonlive <soon@live.cn>
2017-04-17 16:52:56 +0800

    chore: enable HotModuleReplacementPlugin

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -71,7 +71,7 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin({ filename: '[name].css', allChunks: true }),
-    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
